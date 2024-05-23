61ea2372a02a022c2cd081c02e233c3dc331992f
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2024-05-23 16:10:52 -0700

    Added messages

--- src/App.js
+++ src/App.js
@@ -4,25 +4,24 @@ import Footer from "./components/Footer";
 import { Loader } from "react-feather";
 import Quotes from "./components/quotes/Quotes";
 import FavoriteQuotes from "./components/quotes/FavoriteQuotes.js";
+import Message from "./components/Message.js";
 import "./App.css";
 
 function App() {
-  //create two states
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState("All");
   const [favoriteQuotes, setFavoriteQuotes] = useState([]);
+  const [messageText, setMessageText] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
 
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
 
-  //create a variable for categories
   const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
 
-  //create a new variable called maxFaves
   const maxFaves = 3;
 
-  //create async funciton with try/catch block
   const fetchQuotes = async () => {
     try {
       setLoading(true);
@@ -50,26 +49,35 @@ function App() {
     const alreadyFavorite = favoriteQuotes.find(favorite => favorite.id === selectedQuote.id);
 
     if (alreadyFavorite) {
-      console.log("This quote is already in your favorites!  Choose Another.")
-    } else if (favoriteQuotes.length < maxFaves) {
-      setFavoriteQuotes([...favoriteQuotes, selectedQuote])
-      console.log("Added to favorites!")
+      setMessageText("This quote is already in your favorites!  Choose Another.")
+      setShowMessage(true);
     } else {
-      console.log("Max number of Favorite Quotes reached. Please delete one to add another!")
-    };
+      if (favoriteQuotes.length < maxFaves) {
+        setMessageText("Added to favorites!");
+        setShowMessage(true);
+        setFavoriteQuotes([...favoriteQuotes, selectedQuote]);
+      } else {
+        setMessageText("Max number of Favorite Quotes reached. Please delete one to add another!");
+        setShowMessage(true);
+      }
+    }
   };
-  // console.log(quotes);
 
   const removeFromFavorites = (quoteId) => {
     const updatedFavorites = favoriteQuotes.filter((quote) => quote.id !== quoteId);
     setFavoriteQuotes(updatedFavorites);
   };
 
+  const removeMessage = () => {
+    setShowMessage(false);
+  };
+
   return (
     <div className='App'>
+      {showMessage && <Message messageText={messageText} removeMessage={removeMessage} />}
       <Header />
       <main>
-        <FavoriteQuotes favoriteQuotes={favoriteQuotes} maxFaves={maxFaves} removeFromFavorites={removeFromFavorites}/>
+        <FavoriteQuotes favoriteQuotes={favoriteQuotes} maxFaves={maxFaves} removeFromFavorites={removeFromFavorites} />
         {loading ? (
           <Loader />
         ) :
